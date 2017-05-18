a3bd4fad514db3ed5bb416be3426624c86ce9f0b
Matthew Corrigan <corrigan1247@students.d211.org>
2017-05-18 00:23:10 +0000

    Fixed Google Verification File

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.get('/favicon.ico', function(req, res) {
   res.sendfile(__dirname + "/favicon.ico");
 });
 
-app.get("googleb72043df3ff44b11.html", function(req, res) {
-  res.sendfile("googleb72043df3ff44b11.html");
+app.get("/googleb72043df3ff44b11.html", function(req, res) {
+  res.sendfile(__dirname + "/googleb72043df3ff44b11.html");
 });
 
 function subscribe(email, firstName, lastName) {
