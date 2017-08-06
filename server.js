4fd715825033f2fd13168f2d7244d227b7c64b9a
Matthew Corrigan <corrigan1247@students.d211.org>
2017-08-06 02:38:30 +0000

    Finalized Projects Code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var needle = require("needle");
 var bcrypt = require("bcrypt");
 var bodyParser = require('body-parser');
 var cookie = require("cookie");
+var fs = require("fs");
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -12,6 +13,8 @@ var connection = mysql.createConnection({
   password : '6wzazXi8NV',
   database : 'sql9184046'
 });
+
+var projectString = '<div class="project"><p class="users">Made by: {users}</p><hr><div class="content"><h3>{title}</h3><p>{desc}</p></div><hr><div class="icon upvote" onclick="upvote({id})"><i class="fa fa-thumbs-up"></i> Upvote ({upvotes})</div><div class="icon view-website"><a target="_blank" href="{link}"><i class="fa fa-eye"></i> View Website</a></div><div class="icon details"><a href="../project/?id={id}"><i class="fa fa-book"></i> More Details</a></div></div>';
 
 function parseCookies (request) {
     var list = {},
@@ -162,20 +165,33 @@ app.get("/publish", function(req, res) {
   }
 });
 
-var addProject = function(username, name, link, github, desc) {
-  connection.query("INSERT INTO Projects (`Username`, `Title`, `Link`, `GitHub`, `Description`) VALUES (?, ?, ?, ?, ?);", [username, name, link, github, desc], function(err, results) {
-    if (err) { throw err; }
+var addProject = function(u, username, name, link, github, desc, callback) {
+  connection.query("SELECT Title FROM Projects WHERE Username=?", [u],function(err, results) {
+    
+    if (results.length > 0) {
+      callback(false);
+    } else {
+      connection.query("INSERT INTO Projects (`Username`, `Title`, `Link`, `GitHub`, `Description`) VALUES (?, ?, ?, ?, ?);", [username, name, link, github, desc], function(err, results) {
+        if (err) { throw err; }
+        callback(true);
+      });
+    }
   });
 }
 
 app.post('/publishProj', function(req, res) {
   var user = assertLoggedIn(req, res);
   if(user !== undefined) {
-    if (req.body.pname && req.body.plink && req.body.pgithub && req.body.pdesc) {
-      var added = addProject(user.username, req.body.pname, req.body.plink, req.body.pgithub, req.body.pdesc);
-      res.redirect("../dashboard");
+    if (req.body.pname && req.body.plink && req.body.pgithub && req.body.pdesc && req.body.pauthors) {
+      addProject(user.username, req.body.pauthors, req.body.pname, req.body.plink, req.body.pgithub, req.body.pdesc.replace(/\n/g, "<br>"), function(added) {
+        if (added) {
+          res.redirect("../dashboard");
+        } else {
+          res.redirect("../publish/?error=2")
+        }
+      });
     } else {
-      res.redirect("../publish?error=1");
+      res.redirect("../publish/?error=1");
     }
   } else {
     res.redirect("../login");
@@ -223,14 +239,137 @@ app.post('/login', function(req, res) {
   }
 });
 
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                var item = this[i];
+
+                if((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle) > -1;
+};
+
+function putInInfo(string, results, i, user) {
+  var s = string.replace("{title}", results[i].Title).replace("{upvotes}", results[i].Upvotes).replace("{desc}", results[i].Description.substring(0, 200) + "...").replace("{description}", results[i].Description).replace("{link}", results[i].Link).replace("{link}", results[i].Link).replace("{users}", results[i].Username).replace("{id}", results[i].Id).replace("{id}", results[i].Id);
+  
+  if (user !== undefined) {
+    var upvoters = results[i].Upvoters.split("|");
+    if (contains.call(upvoters, user.username)) {
+      s = s.replace("Upvote", "Upvoted");
+    }
+  }
+  
+  return s;
+}
+
+function getDashboard(req, callback) {
+  
+  connection.query("SELECT * FROM Projects ORDER BY Upvotes", [], function(err, results) {
+  
+    var fullProjectsString = "";
+  
+    var user = assertLoggedIn(req, undefined);
+  
+    for (var i = 0; i < results.length; i++) {
+      var s = putInInfo(projectString, results, i, user);
+      
+      fullProjectsString += s;
+    }
+  
+    fs.readFile('./public/dashboard/dashboard.html', 'utf8', function (err,data) {
+      if (err) {
+        console.log(err);
+        return "500 Server Error"
+      }
+      
+      callback(data.replace("{projects}", fullProjectsString));
+      
+    });
+  });
+}
+
 app.get('/dashboard/', function(req, res) {
   var user = assertLoggedIn(req, res);
   if(user !== undefined) {
-    res.sendfile("./public/dashboard/dashboard.html");
+    getDashboard(req, function(data) {
+      res.send(data);
+    })
   } else {
     res.redirect("../login");
   }
 });
+
+function upvote(req, res, id, sid) {
+  connection.query("SELECT * FROM Projects WHERE Id=?", [id], function(err, results) {
+    var upvoters = results[0].Upvoters.split("|");
+    var user = assertLoggedIn(req, res);
+    if (user == undefined) { return; }
+    for (var i = 0; i < upvoters.length; i++) {
+      if (upvoters[i] == user.username) {
+        return;
+      }
+    }
+    
+    connection.query("UPDATE Projects SET Upvotes=?, Upvoters=? WHERE Id=?", [results[0].Upvotes+1, results[0].Upvoters + user.username + "|",id], function(err, results) {})
+  });
+}
+
+app.post("/upvote", function(req, res) {
+  var id = req.body.id;
+  var sid = req.body.sid;
+  
+  upvote(req, res, id, sid);
+  res.redirect("back");
+});
+
+function getProject(req, results, callback) {
+  
+    var user = assertLoggedIn(req, undefined);
+    
+    fs.readFile('./public/project/project.html', 'utf8', function (err,data) {
+      if (err) {
+        console.log(err);
+        return "500 Server Error"
+      }
+      
+      callback(putInInfo(data, results, 0, user));
+      
+    });
+}
+
+app.get("/project", function(req, res) {
+  var id = req.query.id;
+  if (id == undefined) {
+    res.redirect("../dashboard");
+    return;
+  }
+  
+  connection.query("SELECT * FROM Projects WHERE Id=?", [id], function(err, results) {
+    
+    if (results.length == 0) {res.redirect("../dashboard");return;}
+    
+    getProject(req, results, function(string) {
+      res.send(string);
+    });
+  });
+})
 
 app.listen(process.env.PORT, function() {
   console.log('[+] Started Server');
